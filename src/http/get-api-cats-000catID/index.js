eddf9b27e0ee00594f85b9c1ba315a26a73c257a
Himadri Kakran <Soni@Himadris-MBP.fios-router.home>
2019-07-07 13:06:33 -0400

    Working redirection for linkId path

--- src/http/get-api-cats-000catID/index.js
+++ src/http/get-api-cats-000catID/index.js
@@ -1,18 +1,11 @@
 let data = require('@begin/data')
 
 exports.handler = async function getCats(req) {
-  // <meta http-equiv="Refresh" content="5; url=https://www.w3docs.com" />
-  // window.location.href = "https://www.w3docs.com";
-  if (req.params.linkID == 'soni') {
-    window.location.href = "https://www.w3docs.com";
-  } else if (req.params.linkID == 'karan') {
-    window.location.href = "https://www.medium.com";
+  let cat = await data.get({
+    table: 'cats',
+    key: req.params.catID
+  })
+  return {
+    body: JSON.stringify(cat)
   }
-  // let cat = await data.get({
-  //   table: 'cats',
-  //   key: req.params.catID
-  // })
-  // return {
-  //   body: JSON.stringify(cat)
-  // }
 }
---
+++ src/http/get-linkid-000linkID/index.js
@@ -0,0 +1,48 @@
+// Enable secure sessions, express-style middleware, and more:
+// https://docs.begin.com/en/functions/http/
+//
+// let begin = require('@architect/functions')
+
+// HTTP function
+exports.handler = async function http(req) {
+  console.log(req)
+  let url;
+  if (req.params.linkID == 'soni') {
+    url = "https://www.w3docs.com";
+  } else if (req.params.linkID == 'karan') {
+    url = "https://www.medium.com";
+  }
+
+  let url = await data.get({
+    table: 'links',
+    key: req.params.linkID
+  })
+  // to redirect by simulating a click
+  // window.location.href = url;
+  let html = `
+  <!doctype html>
+  <html lang=en>
+    <head>
+      <meta charset=utf-8>
+      <title>Hi!</title>
+      <meta http-equiv="Refresh" content="0; url=` + url + `" />
+      <link rel="stylesheet" href="https://static.begin.app/starter/default.css">
+      <link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" rel="icon" type="image/x-icon" />
+    </head>
+    <body>
+      <h1 class="center-text">
+        HyperlinkIt
+      </h1>
+      <p class="center-text">
+        Please wait pattiently .. redirecting... 
+      </p>
+    </body>
+  </html>
+  `
+  return {
+    headers: {
+      'content-type': 'text/html; charset=utf8'
+    },
+    body: html
+  }
+}
