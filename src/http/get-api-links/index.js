b117a5784922732babcce522eaabb9b61774ed68
Himadri Kakran <Soni@Himadris-MacBook-Pro.local>
2019-07-07 16:03:26 -0400

    add getlinks

--- src/http/get-api-links/index.js
+++
@@ -1,8 +0,0 @@
-let data = require('@begin/data')
-
-exports.handler = async function getlinks() {
-  let links = await data.get({table:'links'})
-  return {
-    body: JSON.stringify(links)
-  }
-}
