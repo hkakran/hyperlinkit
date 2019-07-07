e6791070c3fe7e30b3deee39176ad66eabc24284
Himadri Kakran <Soni@Himadris-MacBook-Pro.local>
2019-07-07 16:33:36 -0400

    remove bad directory named routes

--- src/http/delete-api-links-000linkID/index.js
+++
@@ -1,7 +0,0 @@
-// learn more about http functions here: https://arc.codes/guides/http
-exports.handler = async function http(req) {
-  return {
-    headers: {'content-type': 'text/html; charset=utf8'}, 
-    body: '<b>hello world from nodejs<b>'
-  }
-}
