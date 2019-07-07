dc510ef5fe216d7baab9996f57f52d26de96e049
karanuppal <karan.uppal711@gmail.com>
2019-07-07 16:33:52 -0400

    it works

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,64 +1,97 @@
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {method: "getSelection"}, function(selection){
-      fetch('https://raw.githubusercontent.com/fczbkk/css-selector-generator/master/build/css-selector-generator.min.js')
-        .then(response => response.text())
-        .then(text => eval(text))
-        .then((selectionResp) => {
-          console.log("Loaded css selector")
-          let my_selector_generator = new CssSelectorGenerator;
-          console.log("Selection: ", selectionResp)
-          let selection = selectionResp.data
-          if (selection.rangeCount < 0) {
-            return;
-          }
-          let range = selection.getRangeAt(0);
-          if (!range) {
-            return;
-          }
-          containerElement = range.commonAncestorContainer;
-          if (containerElement.nodeType != 1)
-          {
-            containerElement = containerElement.parentNode
-          }
-          console.log("Element: ", containerElement);
-          let selector = my_selector_generator.getSelector(containerElement);
-          console.log("Selector: ", selector);
-          // Send selector to the server
-          document.getElementById('text').innerHTML = selector
-          // text.innerHTML = response.data;
-          // on response show a url in the text box
-        })
+    chrome.tabs.sendMessage(tabs[0].id, {method: "getSelection"}, function(response){
+      console.log("Response: ", JSON.stringify(response));
+      document.getElementById('text').innerHTML = response.data
     })
 });
 
-function runCode(currentWindow) {
-  fetch('https://raw.githubusercontent.com/fczbkk/css-selector-generator/master/build/css-selector-generator.min.js')
-    .then(response => response.text())
-    .then(text => eval(text))
-    .then(() => {
-      console.log("Loaded css selector")
-      let my_selector_generator = new CssSelectorGenerator;
-      let selection = currentWindow.getSelection();
-      if (selection.rangeCount < 0) {
-        return;
-      }
-      let range = selection.getRangeAt(0);
-      if (!range) {
-        return;
-      }
-      containerElement = range.commonAncestorContainer;
-      if (containerElement.nodeType != 1)
-      {
-        containerElement = containerElement.parentNode
-      }
-      console.log("Element: ", containerElement);
-      let selector = my_selector_generator.getSelector(containerElement);
-      console.log("Selector: ", selector);
-      // Send selector to the server
+// chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+//   chrome.tabs.executeScript(
+//       tabs[0].id,
+//       {code: 'window.getSelection();'},
+//       function(selection){
+//         fetch('https://raw.githubusercontent.com/fczbkk/css-selector-generator/master/build/css-selector-generator.min.js')
+//           .then(response => response.text())
+//           .then(text => eval(text))
+//           .then((selection) => {
+//             console.log("Loaded css selector")
+//             let my_selector_generator = new CssSelectorGenerator;
+//             console.log("Selection: ", selection)
+//             if (selection.rangeCount < 0) {
+//               return;
+//             }
+//             let range = selection.getRangeAt(0);
+//             if (!range) {
+//               return;
+//             }
+//             containerElement = range.commonAncestorContainer;
+//             if (containerElement.nodeType != 1)
+//             {
+//               containerElement = containerElement.parentNode
+//             }
+//             console.log("Element: ", containerElement);
+//             let selector = my_selector_generator.getSelector(containerElement);
+//             console.log("Selector: ", selector);
+//             // Send selector to the server
+//             document.getElementById('text').innerHTML = selector
+//             // text.innerHTML = response.data;
+//             // on response show a url in the text box
+//           })
+//       }
+//     );
+//   });
 
-      // on response show a url in the text box
-    })
-}
+  // chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+  //   chrome.tabs.executeScript(
+  //       tabs[0].id,
+  //       {code:  'console.log("I am here 1") \
+  //           let my_selector_generator = new CssSelectorGenerator; \
+  //           console.log("I am here 2") \
+  //           let selection = window.getSelection(); \
+  //           console.log("I am here 3")  \
+  //           console.log("Selection: ", selection); \
+  //           console.log("I am here 4") \
+  //           '
+  //       },
+  //       function(selector) {
+  //         document.getElementById('text').innerHTML = JSON.stringify(selector)
+  //       }
+  //     );
+  //   });
+
+
+  // chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+  //   chrome.tabs.executeScript(
+  //       tabs[0].id,
+  //       {code:  'window.getSelection().toString();'},
+  //       function(selection){
+  //         document.getElementById('text').innerHTML = selection
+  //       }
+  //     );
+  //   });
+
+// function runCode() {
+//   return "fetch('https://raw.githubusercontent.com/fczbkk/css-selector-generator/master/build/css-selector-generator.min.js') \
+//     .then(response => response.text()) \
+//     .then(text => eval(text))  \
+//     .then(() => { \
+//       let my_selector_generator = new CssSelectorGenerator; \
+//       let selection = window.getSelection(); \
+//       if (selection.rangeCount < 0) { \
+//         return; \
+//       } \
+//       let range = selection.getRangeAt(0); \
+//       if (!range) { \
+//         return; \
+//       } \
+//       containerElement = range.commonAncestorContainer; \
+//       if (containerElement.nodeType != 1) \
+//       { \
+//         containerElement = containerElement.parentNode \
+//       } \
+//       let selector = my_selector_generator.getSelector(containerElement); \
+//     })\";
+// }
 
 // fetch('https://raw.githubusercontent.com/fczbkk/css-selector-generator/master/build/css-selector-generator.min.js')
 //   .then(response => response.text())
--- chrome_extension/selection.js
+++ chrome_extension/selection.js
@@ -1,6 +1,32 @@
+fetch('https://raw.githubusercontent.com/fczbkk/css-selector-generator/master/build/css-selector-generator.min.js')
+  .then(response => response.text())
+  .then(text => eval(text))
+  .then(() => {console.log("Loaded css generator")});
+
+
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.method == "getSelection")
-    sendResponse({data: window.getSelection()});
+  {
+      let my_selector_generator = new CssSelectorGenerator;
+      let selection = window.getSelection();
+      console.log("Selection: ", selection)
+      if (selection.rangeCount < 0) {
+        return;
+      }
+      let range = selection.getRangeAt(0);
+      if (!range) {
+        return;
+      }
+      containerElement = range.commonAncestorContainer;
+      if (containerElement.nodeType != 1)
+      {
+        containerElement = containerElement.parentNode
+      }
+      console.log("Element: ", containerElement);
+      let selector = my_selector_generator.getSelector(containerElement);
+      console.log("Selector: ", selector);
+      sendResponse({data: selector});
+  }
   else
     sendResponse({}); // snub them.
 });
