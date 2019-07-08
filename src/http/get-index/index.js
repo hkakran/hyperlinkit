fc208cf6c0d3e23961a2e563a48ac4e0dd03529f
Himadri Kakran <Soni@Himadris-MBP.fios-router.home>
2019-07-07 22:56:35 -0400

    add back bad test

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -1,13 +1,17 @@
 let arc = require('@architect/functions')
-
 /**
  * arc.proxy.public
  *
  * - a lambda function helper that serves files in /public
  * - very configurable! documented here: https://arc.codes/guides/spa
  */
-
-exports.handler = arc.proxy.public({spa:true})
+// exports.handler = async function http(request) {
+//   return {
+//     type: 'text/html',
+//     body: '<h1>Hello World! 🎉</h1>'
+//   }
+// }
+exports.handler = arc.proxy.public({spa: true});
 // exports.handler = arc.proxy.public({
 //   spa: false, //return 404 if route not found
 //   plugins:  {
